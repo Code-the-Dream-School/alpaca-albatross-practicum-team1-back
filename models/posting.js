b1831bcc2f18ffe6b9c7bccfc8f732c18b7d38a2
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-10-30 16:09:20 -0700

    fix (#16)
    pretttier

--- models/posting.js
+++ models/posting.js
@@ -1,27 +1,27 @@
 const PostingSchema = new mongoose.Schema(
-  {
-    userName: {
-      type: String,
-      required: [true, "Please provide name"],
-      maxlength: 50,
+    {
+        userName: {
+            type: String,
+            required: [true, 'Please provide name'],
+            maxlength: 50,
+        },
+        postMessage: {
+            type: String,
+            required: [true, 'Please provide a posting'],
+            maxlength: 200,
+        },
+        status: {
+            type: String,
+            enum: ['current', 'fulfilled'],
+            default: 'current',
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide user'],
+        },
     },
-    postMessage: {
-      type: String,
-      required: [true, "Please provide a posting"],
-      maxlength: 200,
-    },
-    status: {
-      type: String,
-      enum: ["current", "fulfilled"],
-      default: "current",
-    },
-    createdBy: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-      required: [true, "Please provide user"],
-    },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+)
 
-module.exports = mongoose.model("Posting", PostingSchema);
+module.exports = mongoose.model('Posting', PostingSchema)
--- models/user.js
+++ models/user.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    maxlength: 50,
-    minlength: 2,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    minlength: 6,
-    required: [true, "Please provide password"],
-  },
-});
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        maxlength: 50,
+        minlength: 2,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: [true, 'Please provide password'],
+    },
+})
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
