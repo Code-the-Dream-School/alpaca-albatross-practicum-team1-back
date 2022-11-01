4e67a1f94b734a5600ebd73d4d3ff0fff0325630
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-10-31 19:55:52 -0700

    fixed json

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Volunteer Page!')
 })
 app.post('/posting', (req, res) => {
-    res.status(200).send({ isSuccess: true })
+    res.status(200).json({ isSuccess: true })
 })
 
 app.listen(port, () => {
