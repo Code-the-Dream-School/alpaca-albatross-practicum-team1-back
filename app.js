5793d72d90315ef626d41db347764497c8a430bf
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-11-04 17:13:28 -0700

    worked db

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 const express = require('express')
 const app = express()
-
 const port = 3000
+const mongoose = require('mongoose')
 
+const url = 'mongodb://127.0.0.1:27017/volunteer'
+mongoose.connect(url).then(console.log('`MongoDB Connected: ${url}'))
 //routers
 const authRouter = require('./routes/Auth')
 const postingRouter = require('./routes/Posting')
 
 // TODO: move to env file & get proper dev/prod url(depending on environment)
-const MongoClient = require('mongodb').MongoClient
-
-const url = 'mongodb://127.0.0.1:27017'
-
-MongoClient.connect(
-    url,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    (err, client) => {
-        if (err) {
-            return console.log(err)
-        }
-        const db = client.db('volunteer')
-
-        console.log(`MongoDB Connected: ${url}`)
-    }
-)
 
 app.use(express.json())
 
--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,8 @@
-const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1:27017/volunteer'
+// const mongoose = require('mongoose')
+// const url = 'mongodb://127.0.0.1:27017/volunteer'
 
-mongoose.connect(url).then(console.log('`MongoDB Connected: ${url}'))
+// const connectDB = (url) => {
+//     return mongoose.connect(url).then(console.log('`MongoDB Connected: ${url}'))
+// }
+
+// module.exports = connectDB
