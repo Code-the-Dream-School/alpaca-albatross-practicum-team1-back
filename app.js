b0ab36b85ee987ac6a7479fd2aafba8666169aad
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-11-29 19:45:45 -0800

    email service

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3001
 const cors = require('cors')
+const nodemailer = require('nodemailer')
 
 app.use(express.json())
 app.use(cors())
@@ -20,6 +21,28 @@ mongoose
     .connect('mongodb://127.0.0.1:27017/volunteer', { useNewUrlParser: true })
     .then(console.log('connection successful'))
     .catch((error) => console.error(error))
+
+//nodemailer
+const transporter = nodemailer.createTransport({
+    server: 'yahoo',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+const mailOptions = {
+    from: 'volunteerboardapp@yahoo.com',
+    to: 'perryl17@gmail.com',
+    subject: 'A kind soul applied to your post',
+    text: 'it works'
+}
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        console.log('error occured', err)
+    } else {
+        console.log('email sent')
+    }
+})
 
 const start = async () => {
     try {
