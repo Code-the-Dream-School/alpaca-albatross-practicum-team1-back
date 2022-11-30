eb4739ce68f1c73b5f7de3c280bf8788a8c146ef
Armani Johnson <89354548+Armanchi@users.noreply.github.com>
2022-11-30 16:25:17 -0600

    authentication for endpoints (#38)
    * authentication for endpoints
    
    * fixed endpoint auth
    
    * fixed auth routes
    
    * fixed routes
    
    * fixed routes
    
    * fixed routes

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+const authenticateUser = require('./middleware/authentication')
+
 // routers
 const authRouter = require('./routes/auth')
 const postingRouter = require('./routes/Posting')
+const authPostingRouter = require('./routes/authPosting')
+
 // routes
 app.use('/auth', authRouter)
 app.use('/post', postingRouter)
+app.use('/post', authenticateUser, authPostingRouter)
 
 // TODO: move to env file & get proper dev/prod url(depending on environment)
 mongoose
--- routes/Posting.js
+++ routes/Posting.js
@@ -1,17 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const {
-    get,
-    create,
-    getPost,
-    updatePost,
-    applicantPost
-} = require('../controllers/posting')
+const { get } = require('../controllers/posting')
 
 router.route('/getPosts').get(get)
-router.route('/createPost').post(create)
-router.route('/getPost').get(getPost)
-router.route('/updatePost').patch(updatePost)
-router.route('/apply').post(applicantPost)
 
 module.exports = router
