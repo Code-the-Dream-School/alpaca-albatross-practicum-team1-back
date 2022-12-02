375c6d34001419bb52c2d2d8fe6387cee55a9a8d
Armanchi <armanijohnson0104@gmail.com>
2022-12-02 15:27:20 -0600

    auth validate

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+
 const authenticateUser = require('../middleware/authentication')
 
 const { postRegister, postLogin } = require('../controllers/auth')
