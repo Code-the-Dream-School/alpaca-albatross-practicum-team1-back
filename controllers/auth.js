adef1959ea0136ced4de47d39700ed67271f55fd
Armani Johnson <89354548+Armanchi@users.noreply.github.com>
2022-12-05 19:41:05 -0600

    validateToken (#41)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const { register } = require('../services/register')
 const { StatusCodes } = require('http-status-codes')
 const { login } = require('../services/login')
+const authenticateUser = require('../middleware/authentication')
+const User = require('../models/user')
 
 async function postRegister(req, res, next) {
     try {
@@ -15,9 +17,9 @@ async function postRegister(req, res, next) {
 
 async function postLogin(req, res, next) {
     try {
-        const token = await login(req)
+        const user = await login(req)
         res.status(StatusCodes.OK).json({
-            token
+            user
         })
     } catch (error) {
         res.status(StatusCodes.UNAUTHORIZED).json({
@@ -25,4 +27,25 @@ async function postLogin(req, res, next) {
         })
     }
 }
-module.exports = { postRegister, postLogin }
+
+async function validateToken(req, res, next) {
+    try {
+        authenticateUser(req, res)
+        const user = await User.findById(req.user.userId)
+        res.status(StatusCodes.OK).json({
+            user: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+            error: error.message
+        })
+        next(error)
+    }
+}
+
+module.exports = { postRegister, postLogin, validateToken }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { postRegister, postLogin } = require('../controllers/auth')
+const {
+    postRegister,
+    postLogin,
+    validateToken
+} = require('../controllers/auth')
 
 router.post('/register', postRegister)
 router.post('/login', postLogin)
+router.route('/validateToken').get(validateToken)
 
 module.exports = router
