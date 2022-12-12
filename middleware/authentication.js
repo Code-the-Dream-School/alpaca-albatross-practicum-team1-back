b90a7be2da5b5a70f8474b2d60b6e31ddf5604f8
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-12-11 18:15:15 -0800

    added back env.dev

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,7 +4,6 @@ const { UnauthenticatedError } = require('../errors/UnauthenticatedError')
 
 const auth = async (req, res) => {
     const authHeader = req.headers.authorization
-    console.log(authHeader)
     if (!authHeader || !authHeader.startsWith('Bearer')) {
         throw new UnauthenticatedError('Authentication invalid')
     }
