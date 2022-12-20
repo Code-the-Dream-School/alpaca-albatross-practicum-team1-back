062ebd6c69b30c8850861464400d44b8c3853e4d
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-12-19 17:05:03 -0800

    error handeling (#55)

--- controllers/posting.js
+++ controllers/posting.js
@@ -2,13 +2,15 @@ const Posting = require('../models/posting')
 const User = require('../models/user')
 const mongoose = require('mongoose')
 const postingService = require('../services/posting')
+const { StatusCodes } = require('http-status-codes')
+const UnauthenticatedError = require('../errors/UnauthenticatedError')
 
 async function get(req, res, next) {
     try {
         const posts = await postingService.getAllPosts(req)
-        res.json({ posts: posts })
+        res.status(StatusCodes.OK).json({ posts: posts })
     } catch (error) {
-        res.status(500).json({
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: error.message
         })
         next(error)
@@ -18,11 +20,17 @@ async function get(req, res, next) {
 async function create(req, res, next) {
     try {
         const post = await postingService.createPost(req)
-        res.json({ id: post._id.toString() })
+        res.status(StatusCodes.OK).json({ id: post._id.toString() })
     } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
+        if (error.name == 'UnauthenticatedError') {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                error: error.message
+            })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: error.message
+            })
+        }
         next(error)
     }
 }
@@ -30,32 +38,52 @@ async function create(req, res, next) {
 const getPost = async (req, res, next) => {
     try {
         const post = await postingService.getPosts(req)
-        res.json({ post: post })
+        res.status(StatusCodes.OK).json({ post: post })
     } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
+        if (error.name == 'UnauthenticatedError') {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                error: error.message
+            })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: error.message
+            })
+        }
         next(error)
     }
 }
 const updatePost = async (req, res) => {
     try {
         const post = await postingService.updatesPost(req)
-        res.json({ post: post })
+        res.status(StatusCodes.OK).json({ post: post })
     } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
+        if (error.name == 'UnauthenticatedError') {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                error: error.message
+            })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: error.message
+            })
+        }
+        next(error)
     }
 }
 const applicantPost = async (req, res) => {
     try {
         const post = await postingService.applicantsPost(req)
-        res.json({ post: post })
+        res.status(StatusCodes.OK).json({ post: post })
     } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
+        if (error.name == 'UnauthenticatedError') {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                error: error.message
+            })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: error.message
+            })
+        }
+        next(error)
     }
 }
 
